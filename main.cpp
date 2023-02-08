c5a7193112ca43103ca6e7f1770a48f378a8b6f8
Shikha Bista <shikhabista33@gmail.com>
2023-02-08 22:29:00 +0545

    wip: login

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ using namespace std;
 struct UserInput {
     char Username[500];
     char Password[500];
+    char FirstName[500];
+    char LastName[500];
+    char ContactNo[500];
+    char Address[500];
+    char UserNo[500];
 };
 
 class UserNotFoundException {
@@ -30,16 +35,16 @@ public:
         if (answer == "y" || answer == "Y") {
             cout << "\n\t\tWould you like to login?:";
             cin >> choice;
-            if (choice == "y") {
+            if (choice == "y" || choice == "Y") {
                 return Login();
             } else {
-                cout<<"Only (Y)es or (N)o are acceptable";
+                cout << "Only (Y)es or (N)o are acceptable";
             }
         }
         if (answer == "n" || answer == "N") {
-            cout << "Would you like to register a new user?:";
+            cout << "\n\t\tWould you like to register a new user?:";
             cin >> choice;
-            if (choice == "y") {
+            if (choice == "y" || choice == "Y") {
                 Register();
             } else {
                 return false;
@@ -61,8 +66,13 @@ public:
             bool userNameMatched = strcmp(user.Username, tempUser.Username) == 0;
             bool passwordMatched = strcmp(user.Password, tempUser.Password) == 0;
             if (userNameMatched && passwordMatched) {
-                cout<<"Welcome!";
+                cout << "\t\tLogged in successfully\nWelcome!";
+                cout<<"|";
                 return true;
+            }
+            else {
+                cout<<"\n\t\tSorry! Invalid credentials";
+                return false;
             }
         }
         return false;
@@ -74,7 +84,16 @@ public:
         fstream authFile; // fstream => file stream
         // open ( fileName
         authFile.open(authFileName, ios::app | ios::binary);
-
+        cout << "\t\t\tFirst Name:";
+        cin >> user.FirstName;
+        cout << "\t\t\tLast Name:";
+        cin >> user.LastName;
+        cout << "\t\t\tContact No:";
+        cin >> user.ContactNo;
+        cout << "\t\t\tAddress:";
+        cin >> user.Address;
+        cout << "\t\t\tUser No:";
+        cin >> user.UserNo;
         cout << "\t\tUsername: \t";
         cin >> user.Username;
         cout << "\t\tPassword: \t";
@@ -86,12 +105,10 @@ public:
 };
 
 
-
-
 int main() {
-    cout<<"****************** * Shinoga Departmental Store * ******************\n";
-    cout<< "\t\t\t-- Birtamode, Jhapa -- \n";
-    cout<<"\n\t\tWELCOME TO SHINOGA DEPARTMENTAL STORE";
+    cout << "****************** * Shinoga Departmental Store * ******************\n";
+    cout << "\t\t\t-- Birtamode, Jhapa -- \n";
+    cout << "\n\t\tWELCOME TO SHINOGA DEPARTMENTAL STORE";
 //    cout<< "\t\t\tMain Menu\n";
 
     Authentication auth;
