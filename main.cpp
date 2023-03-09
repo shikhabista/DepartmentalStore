a2de72d29c3246e6d0bc45b83753456ca0fac78e
Shikha Bista <shikhabista33@gmail.com>
2023-03-09 22:41:34 +0545

    chore: fix some problems

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <cstring>
 #include <vector>
-
+#include <conio.h>
 using namespace std;
 
 struct UserInput {
@@ -58,7 +58,7 @@ public:
                     break;
             }
             char shouldContinue;
-            cout << "\tPress Y/y to continue. Any other key to exit: ";
+            cout << "\t\tPress Y/y to continue. Any other key to exit: ";
             cin >> shouldContinue;
             if (shouldContinue != 'Y' && shouldContinue != 'y') {
                 return false;
@@ -70,8 +70,8 @@ public:
         UserInput user, tempUser;
         fstream authFile;
         authFile.open(authFileName, ios::in | ios::binary);
-        cout << "\t\t------------------------------------------------\n";
-        cout << "\n\t\tUsername:";
+        cout << "\t\t------------------------------------------------\n\n";
+        cout << "\t\tUsername:";
         cin >> user.Username;
         cout << "\t\tPassword:";
         cin >> user.Password;
@@ -80,7 +80,7 @@ public:
             bool userNameMatched = strcmp(user.Username, tempUser.Username) == 0;
             bool passwordMatched = strcmp(user.Password, tempUser.Password) == 0;
             if (userNameMatched && passwordMatched) {
-                cout << "\n\t\t************ Logged in successfully ************\n";
+                cout << "\n\t\t****************** Logged in successfully ************\n";
                 cout << "\n\t\t Welcome! What would you like to do?\n";
                 return true;
             } else {
@@ -97,9 +97,9 @@ public:
         fstream authFile; // fstream => file stream
         authFile.open(authFileName, ios::app | ios::binary);
 
-        cout << "\tUsername:";
+        cout << "\t\tUsername:";
         cin >> user.Username;
-        cout << "\tPassword:";
+        cout << "\t\tPassword:";
         cin >> user.Password;
         // write ( (char *) &user, sizeof(UserInput) ) sizeof rollNo | sizeof(int)
         authFile.write((char *) &user, sizeof(UserInput));
@@ -113,37 +113,41 @@ class Article {
 protected:
 
     void DisplayProducts() {
-        cout << "S.N \t Article No \t Article Name \n";
 
+        cout<< "\t__________________________________________________________________________________________\n";
+        cout << "\tS.N \t | Article No \t | Article Name \t | Unit \t | CP \t\t | SP \t | \n";
+        cout<< "\t__________________________________________________________________________________________\n";
         fstream file;
         ProductInfo productInfo{};
         file.open(ProductFileName, ios::in | ios::binary);
         int count = 0;
         while (file.read((char *) &productInfo, sizeof(ProductInfo))) {
-            cout << ++count << "\t" << productInfo.ArticleNo;
+            cout << "\t" << ++count << "\t | " << productInfo.ArticleNo << "\t\t | " << productInfo.ArticleName << "\t\t | "
+            << productInfo.ProductUnit << "\t\t | " << productInfo.ProductCp << "\t\t | " << productInfo.ProductSp << "\t |";
             cout << endl;
         }
 
     }
 
     void CreateProduct() {
-        ProductInfo info;
+        ProductInfo info, pInfo;
         fstream productFile;
         productFile.open(ProductFileName, ios::app | ios::binary);
-        cout << "Enter Product Details to add a product\n";
-        cout << "Article Id:\n";
+        cout << "\t\tEnter Product Details to add a product\n";
+        cout << "\t\tArticle Id:";
         cin >> info.ProductId;
-        cout << "Article Barcode (EAN/BC):\n";
+        cout << endl;
+        cout << "\t\tArticle Barcode (EAN/BC):";
         cin >> info.Barcode;
-        cout << "Article No:\n";
+        cout << "\t\tArticle No:";
         cin >> info.ArticleNo;
-        cout << "Article Description:\n";
+        cout << "\t\tArticle Description:";
         cin >> info.ArticleName;
-        cout << "Unit:\n";
+        cout << "\t\tUnit:";
         cin >> info.ProductUnit;
-        cout << "Purchase Rate:\n";
+        cout << "\t\tPurchase Rate:";
         cin >> info.ProductCp;
-        cout << "Sales Rate:\n";
+        cout << "\t\tSales Rate:";
         cin >> info.ProductSp;
 
         productFile.write((char *) &info, sizeof(ProductInfo));
@@ -152,7 +156,7 @@ protected:
     void UpdateProduct() {
         ProductInfo pInfo, fileData;
         bool found = false;
-        cout << "Enter Article no of the article you would like to update:\t";
+        cout << "\t\tEnter Article no of the article you would like to update:\t";
         cin >> pInfo.ArticleNo;
 
         fstream productFile, tempFile;
@@ -162,6 +166,26 @@ protected:
         while (productFile.read((char *) &fileData, sizeof(ProductInfo))) {
             if (::strcmp(fileData.ArticleNo, pInfo.ArticleNo) == 0) {
                 found = true;
+                cout << "Article Id:" << fileData.ProductId << "\t";
+                cout << "Article Barcode (EAN/BC):" << fileData.Barcode << "\t";
+                cout << "Article No:" << fileData.ArticleNo << "\t\n";
+                cout << "Article Description:" << fileData.ArticleName << "\t";
+                cout << "Unit:" << fileData.ProductUnit << "\t";
+                cout << "Purchase Rate:" << fileData.ProductCp << "\t";
+                cout << "Sales Rate:" << fileData.ProductSp << "\t\n";
+                cout << "Enter Product Details to add a product\n";
+                cout<< "Barcode:";
+                cin >> pInfo.Barcode;
+                cout<< "Article No:";
+                cin >> pInfo.ArticleNo;
+                cout<< "Article Description:";
+                cin >> pInfo.ArticleName;
+                cout<< "Unit:";
+                cin >> pInfo.ProductUnit;
+                cout<< "Purchase Rate:";
+                cin >> pInfo.ProductCp;
+                cout<< "Sales Rate:";
+                cin >> pInfo.ProductSp;
                 tempFile.write((char *) &pInfo, sizeof(ProductInfo));
             } else {
                 tempFile.write((char *) &fileData, sizeof(ProductInfo));
@@ -173,16 +197,16 @@ protected:
         ::remove(ProductFileName.c_str());
         ::rename("temp.dat", ProductFileName.c_str());
         if (found) {
-            cout << "Product details updated successfully" << endl;
+            cout << "\tProduct details updated successfully" << endl;
         } else {
-            cout << "No matching Product found" << endl;
+            cout << "\tNo matching Product found" << endl;
         }
     }
 
     void DeleteProduct() {
         ProductInfo pInfo, fileData;
         bool found = false;
-        cout << "Enter the Article no of the article you would like to delete:\t";
+        cout << "\tEnter the Article no of the article you would like to delete:\t";
         cin >> pInfo.ArticleNo;
 
         fstream authFile, tempFile;
@@ -201,9 +225,9 @@ protected:
         ::remove(ProductFileName.c_str());
         ::rename("temp.dat", ProductFileName.c_str());
         if (found) {
-            cout << "Product deleted successfully." << endl;
+            cout << "\tProduct deleted successfully." << endl;
         } else {
-            cout << "No matching product found";
+            cout << "\tNo matching product found";
         }
 
 
@@ -216,13 +240,14 @@ public:
         while (true) {
             char shouldContinue;
             char choice;
-            cout << "\t1. Create Product" << endl;
-            cout << "\t2. View Product Report" << endl;
-            cout << "\t3. Update Product" << endl;
-            cout << "\t4. Delete Product" << endl;
+            cout << "\t\t1. Create Product" << endl;
+            cout << "\t\t2. View Product Report" << endl;
+            cout << "\t\t3. Update Product" << endl;
+            cout << "\t\t4. Delete Product" << endl;
 
+            cout << "\t\tYour choice:";
             cin >> choice;
-
+            cout << endl;
             switch (choice) {
                 case '1': {
                     CreateProduct();
